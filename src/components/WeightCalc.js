9d639d8786aba3635014b99fd55115a50334f8f1
TandiweP <87207713+TandiweP@users.noreply.github.com>
2022-08-08 22:16:00 -0600

    css & display

--- src/components/WeightCalc.js
+++ src/components/WeightCalc.js
@@ -5,9 +5,9 @@ export default function WeightCalc () {
     
     return (
         <div>
-    <h1> Gross weight is : </h1>
-    <h1> Tare weight is : </h1>
-    <h1> Net weight is : </h1>
+    <h1> Gross weight is ${} </h1>
+    <h1> Tare weight is ${} </h1>
+    <h1> Net weight is ${} </h1>
     </div>
     )
 }
