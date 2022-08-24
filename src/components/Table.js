48094f059051a5fcd51c2af079db9515c543e910
TandiweP <87207713+TandiweP@users.noreply.github.com>
2022-08-24 09:45:48 -0600

    reformat table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,37 @@
 
 import React  from "react";
 
-export default function Table (props) {
+export default function Table ({handleSubmit}) {
 
 return (
     
     <table className = "response">
+        <thead>
         <tr>
+            <th>Load #</th>
             <th>Gross</th>
             <th>Tare</th>
             <th>Net</th>
             <th>Tons</th>
         </tr>
-        <tr>
-            <td>{props.gross}</td>
-            <td>{props.tare}</td>
-            <td>{props.net}</td>
-            <td>{props.tons}</td>
-        </tr>
+        </thead>
+        <tbody>
+            {/* {
+                handleSubmit.map((data, index) => {
+                    return(
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>{data.gross}</td>
+                            <td>{data.tare}</td>
+                            <td>{data.net}</td>
+                            <td>{data.tons}</td>
+                        </tr>
+                    )
+            
+       })
+        } */}
+        Weights
+        </tbody>
     </table>
    )
 
